329c2466d7a46d248aa4a45754120d22e09de5b5
DennisMMugambi <dennis.mutwiri.mugambi@gmail.com>
2023-08-13 17:27:20 +0300

    create Coin model struct to model json response.

--- Crypto-iOS/Models/CoinModel.swift
+++ Crypto-iOS/Models/CoinModel.swift
@@ -9,7 +9,7 @@
  Coin Gecko API markets endpoint
  
  https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h&locale=en
- 
+ .
  */
 
 import Foundation
