878eeadc54afe3c370ed44da60c007ceb5a0198b
DennisMMugambi <dennis.mutwiri.mugambi@gmail.com>
2023-08-13 10:05:29 +0300

    Initial Commit

---
+++ Crypto-iOS/Crypto_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_iOSApp.swift
+//  Crypto-iOS
+//
+//  Created by Dennis Mutwiri Mugambi on 13/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
