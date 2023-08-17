5589b1432f5f24d395838d47a2e5612c541c51ab
Dennis Mugambi <dennis.mutwiri.mugambi@gmail.com>
2023-08-17 14:17:43 +0300

    Add dynamic list of coins from view model

---
+++ Crypto-iOS/Core/Home/ViewModels/HomeViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  HomeViewModel.swift
+//  Crypto-iOS
+//
+//  Created by Dennis Mutwiri Mugambi on 16/08/2023.
+//
+
+import Foundation
+
+class HomeViewModel: ObservableObject {
+    
+    @Published var allCoins: [CoinModel] = []
+    @Published var portfolioCoins: [CoinModel] = []
+    
+    init() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            self.allCoins.append(DeveloperPreview.instance.coin)
+            self.portfolioCoins.append(DeveloperPreview
+                .instance.coin)
+        }
+    }
+}
